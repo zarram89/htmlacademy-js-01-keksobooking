2cfcfcc585d816a206f30e50b99504eea7479818
Ramil <zarram89@gmail.com>
2026-05-06 12:23:43 +0500

    feat: adds form activity

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { getAds } from './data.js';
 import { renderCard } from './card.js';
+import { disablePage, enablePage } from './form.js';
 
 const mapCanvas = document.querySelector('#map-canvas');
 const ads = getAds();
@@ -7,3 +8,6 @@ const ads = getAds();
 if (ads.length > 0) {
   mapCanvas.append(renderCard(ads[0]));
 }
+
+disablePage();
+enablePage();
