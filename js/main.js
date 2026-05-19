399b56aff8728567e1b2ad0be733c03cdb326176
Ramil <zarram89@gmail.com>
2026-05-19 12:11:14 +0500

    feat: adds map

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 import { getAds } from './data.js';
-import { renderCard } from './card.js';
-import { disablePage, enablePage } from './form.js';
+import { disablePage, initSlider } from './form.js';
+import { initMap } from './map.js';
 
-const mapCanvas = document.querySelector('#map-canvas');
 const ads = getAds();
 
-if (ads.length > 0) {
-  mapCanvas.append(renderCard(ads[0]));
-}
-
 disablePage();
-enablePage();
+initSlider();
+initMap(ads);
